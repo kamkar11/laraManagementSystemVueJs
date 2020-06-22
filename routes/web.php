7ef9a258240f0bc07ea63ffebb6c6422fc89d36c
Kamil Machański <kamilmachanski@gmail.com>
2020-06-22 16:14:22 +0200

    clear in code

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
