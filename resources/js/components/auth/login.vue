8a21560775433f9ba473aa8af6a3bd00f24a7a79
Kamil Machański <kamilmachanski@gmail.com>
2020-06-22 19:45:23 +0200

    create login and register components

---
+++ resources/js/components/auth/login.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Login form</h1>
+</template>
+
+<script type="text/javascript">
+
+</script>
+
+<script type="text/css">
+
+</script>
---
+++ resources/js/components/auth/register.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Register form</h1>
+</template>
+
+<script type="text/javascript">
+
+</script>
+
+<script type="text/css">
+
+</script>
