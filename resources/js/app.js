60e015e2e306ab73e3d13866947f9f06a3656515
Kamil Machański <kamilmachanski@gmail.com>
2020-06-23 20:44:59 +0200

    validate login fields

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,24 @@ import { routes } from './routes';
 import User from './helpers/User';
 window.User = User;
 
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+});
+
+window.Toast = Toast;
+
+
 const router = new VueRouter({
     routes,
     mode: 'history'
--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -16,10 +16,12 @@
                                         <div class="form-group">
                                             <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp"
                                                    placeholder="Enter Email Address"  v-model="form.email" >
+                                            <small class="text-danger" v-if="errors.email">{{ errors.email[0] }}</small>
 
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password" v-model="form.password"  >
+                                            <small class="text-danger" v-if="errors.password">{{ errors.password[0] }}</small>
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
@@ -65,7 +67,9 @@
                 form: {
                     email: null,
                     password: null
-                }
+                },
+
+                errors: {}
             }
         },
 
@@ -74,9 +78,21 @@
                 axios.post('/api/auth/login', this.form)
                 .then(res => {
                     User.responseAfterLogin(res);
-                    this.$router.push({ name: 'home'})
+
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Signed in successfully'
+                    });
+
+                    this.$router.push({ name: 'home'});
                 })
-                .catch(error => console.log(error.response.data))
+                .catch(error => this.errors = error.response.data.errors)
+                .catch(
+                    Toast.fire({
+                        icon: 'warning',
+                        title: 'Invalid Email or Password'
+                    })
+                )
             }
         }
     }
