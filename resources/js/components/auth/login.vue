66c80176c6145244421a9361c040040dad0e5317
Kamil Machański <kamilmachanski@gmail.com>
2020-06-23 20:19:15 +0200

    add home component and security login

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -53,6 +53,13 @@
 <script>
 
     export default {
+
+        created() {
+            if (User.loggedIn()){
+                this.$router.push({ name: 'home'})
+            }
+        },
+
         data() {
             return {
                 form: {
@@ -65,7 +72,10 @@
         methods: {
             login() {
                 axios.post('/api/auth/login', this.form)
-                .then(res => User.responseAfterLogin(res))
+                .then(res => {
+                    User.responseAfterLogin(res);
+                    this.$router.push({ name: 'home'})
+                })
                 .catch(error => console.log(error.response.data))
             }
         }
