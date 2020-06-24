cbc468397c93f43d473407745c347a9d7ffa394e
Kamil Machański <kamilmachanski@gmail.com>
2020-06-24 19:59:35 +0200

    create routes employee module

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,8 +2,11 @@
 let login = require('./components/auth/login.vue').default;
 let register = require('./components/auth/register.vue').default;
 let forget = require('./components/auth/forget.vue').default;
-let home = require('./components/home.vue').default;
 let logout = require('./components/auth/logout.vue').default;
+
+let home = require('./components/home.vue').default;
+
+let storeemployee = require('./components/employee/create.vue').default;
 
 export const routes = [
     { path: '/', component: login, name: '/' },
@@ -11,5 +14,6 @@ export const routes = [
     { path: '/forget', component: forget, name: 'forget' },
     { path: '/home', component: home, name: 'home' },
     { path: '/logout', component: logout, name: 'logout' },
+    { path: '/store-employee', component: storeemployee, name: 'store-employee' },
 ];
 
