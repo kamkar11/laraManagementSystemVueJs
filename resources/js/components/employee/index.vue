92b1f799b870cd4e3d3ed3a8c8c68e77bd244d4d
Kamil Machański <kamilmachanski@gmail.com>
2020-06-26 23:17:32 +0200

    add index employee layout

--- resources/js/components/employee/index.vue
+++ resources/js/components/employee/index.vue
@@ -7,25 +7,49 @@
             </div>
         </div>
 
+        <br><br>
 
-        <div class="row justify-content-center">
-            <div class="col-xl-12 col-lg-12 col-md-9">
-                <div class="card shadow-sm my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="login-form">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">All Employee</h1>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
+        <div class="row">
+            <div class="col-lg-12 mb-4">
+                <!-- Simple Tables -->
+                <div class="card">
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 class="m-0 font-weight-bold text-primary">Employee List</h6>
                     </div>
+                    <div class="table-responsive">
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                            <tr>
+                                <th>Name</th>
+                                <th>Photo</th>
+                                <th>Phone</th>
+                                <th>Sallery</th>
+                                <th>Join date</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="employee in employees" :key="employee.id">
+                                <td>{{ employee.name }}</td>
+                                <td><img :src="employee.photo" id="em_photo"></td>
+                                <td>{{ employee.phone }}</td>
+                                <td>{{ employee.sallery }}</td>
+                                <td>{{ employee.joining_date }}</td>
+
+                                <td>
+                                    <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                    <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-footer"></div>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
@@ -39,15 +63,34 @@
             }
         },
 
-
+        data(){
+            return{
+                employees: []
+            }
+        },
 
         methods: {
+            allEmployee(){
+                axios.get('/api/employee/')
+                .then(({data}) => (this.employees = data))
+                .catch()
+            }
+        },
 
+        created(){
+            this.allEmployee();
         }
 
     }
 
 
 </script>
+
+<style type="text/css">
+    #em_photo{
+        height: 40px;
+        width: 40px;
+    }
+</style>
 
 
